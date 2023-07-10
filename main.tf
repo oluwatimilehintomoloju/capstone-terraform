a4868c2c354e6c0ffed5de8456e85913bdfd521d
kolabiz <kolabiz01@gmail.com>
2023-07-10 19:53:02 +0100

    add eks cluster

--- main.tf
+++ main.tf
@@ -22,3 +22,19 @@ resource "aws_ecr_repository" "repo-1" {
     }
 
 }
+
+# Create the EKS cluster
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "capstone-eks-cluster" 
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.27" 
+
+  vpc_config {
+    subnet_ids =  "subnet-07771b22ea9742877"    #  bkss-public-subnet-eu-west-2a     [aws_subnet.eks_subnet.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy_attachment,
+    aws_iam_role_policy_attachment.eks_service_policy_attachment,
+  ]
+}
