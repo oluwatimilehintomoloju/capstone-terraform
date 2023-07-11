744c2bc38900f2e1a4ab3e00c87ab81952c9cb71
Olabode Obe <olabodeobe@olabodes-mbp.home>
2023-07-11 06:05:01 +0100

    Added userdata and ami lookup code snippets

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,6 +1,6 @@
 # Create the EKS cluster
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "capstone-eks-cluster"
+  name     = var.cluster_name
   role_arn = aws_iam_role.cluster_role.arn
   version  = "1.27"
 
---
+++ locals.tf
@@ -0,0 +1,9 @@
+# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
+locals {
+  demo-node-userdata = <<USERDATA
+#!/bin/bash
+set -o xtrace
+/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks_cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.eks_cluster.certificate_authority[0].data}' '${var.cluster_name}'
+USERDATA
+
+}
